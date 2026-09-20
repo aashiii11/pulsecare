d3a264853a1ee929515752b96779258973c6e10b
aashiii11 <aashishrivastav1106@gmail.com>
2026-09-20 22:08:01 +0530

    public folder fix

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();      
-app.use(express.static(path.join(__dirname, 'Frontend')));
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'Frontend', 'index.html')); });    // <-- app pehle banao
+    // <-- app pehle banao
 
 app.use(cors());                // <-- fir cors use karo
 app.use(express.json());
